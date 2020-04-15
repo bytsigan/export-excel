b0f8fc045600a169e5656004a7f56027d5f74796
bytsigan <php2@gerchikco.com>
2020-04-15 15:15:08 +0300

    fix

--- src/ExportExcelServiceProvider.php
+++ src/ExportExcelServiceProvider.php
@@ -56,6 +56,5 @@ class ExportExcelServiceProvider extends ServiceProvider
         $this->app->singleton('export-excel', function () {
             return new ExportExcel;
         });
-        
     }
 }
